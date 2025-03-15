f1cb5610c6321f9f2265d8d563f7c5ff1eeeea71
wwqdrh <huiloademail@163.com>
2025-03-15 21:38:49 +0800

    fix

---
+++ encrypt.sh
@@ -0,0 +1,5 @@
+edge-key encrypt -key 432e8c29b27cb08b3550a49b1f79f750 -in ./assets/jwt_private.pem -out ./assets/jwt_private.data
+edge-key encrypt -key 432e8c29b27cb08b3550a49b1f79f750 -in ./assets/jwt_public.pem -out ./assets/jwt_public.data
+edge-key encrypt -key 432e8c29b27cb08b3550a49b1f79f750 -in ./assets/wxpay_private.pem -out ./assets/wxpay_private.data
+edge-key encrypt -key 432e8c29b27cb08b3550a49b1f79f750 -in ./assets/wxpay_public.pem -out ./assets/wxpay_public.data
+
